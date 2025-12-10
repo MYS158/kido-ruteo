78954a08d5be35e9f2c44843bab71a78388945d9
Miguel Muñoz <emwyes158@gmail.com>
2025-12-10 13:25:52 -0600

    chore: remove old structure for complete KIDO methodology rebuild

--- scripts/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utilidades para scripts de KIDO-Ruteo.
-"""
-
-from . import geo_utils
-
-__all__ = ["geo_utils"]
--- src/kido_ruteo/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Modelos de datos para KIDO-Ruteo.
-"""
-
-from .od_matrix import ODMatrix
-from .zoning import Zoning
-
-__all__ = ["ODMatrix", "Zoning"]
