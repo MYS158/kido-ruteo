113a78fa69dbdc64efc7f5b4c500cebeb18dbffa
Miguel Muñoz <emwyes158@gmail.com>
2025-12-10 13:10:12 -0600

    chore: clean working tree for KIDO-Ruteo 2.0 refactor

--- src/kido_ruteo/__init__.py
+++
@@ -1,4 +0,0 @@
-"""KIDO ruteo pipeline package."""
-
-__all__ = ["pipeline"]
-__version__ = "0.1.0"
--- src/kido_ruteo/config/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Interfaces públicas del módulo de configuración."""
-
-from .loader import Config, ConfigLoader
-
-
-__all__ = ["Config", "ConfigLoader"]
